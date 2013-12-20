a1adfc532dd6b7004d25b53c6fe2c101ff45511f
jack.liu <jackliu@jackliu-ThinkPad-Edge-E420s.(none)>
2013-12-19 22:16:34 -0500

    inode.c added

--- inode.c
+++ inode.c
@@ -1,3 +1,9 @@
 #include"inode.h"
-
-
+#include"lfs.h"
+void print_inodemap(inode_map_t * inodemap){
+   int i = 0;
+   while(i <= 10){
+      printf("inode:%d,%d", inodemap->records[i]);
+      i++
+   }
+}
